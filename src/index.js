aa16ef1818bfa2f2780a2b1808df4d581579499b
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-06-28 21:06:57 +0100

    began adding js and created a home js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("hello world2")
+const content = document.getElementById('content')
+
+const nav = document.createElement('nav')
+const header = document.createElement('header')
+const main = document.createElement('main')
+const div = document.createElement('div')
+const li = document.createElement('li')
+const ul = document.createElement('ul')
+const h2 = document.createElement('h2')
+const liHTML = document.getElementsByTagName('li')
+
+
+home()
+function home () {
+
+    function newli() {
+        return document.createElement('li')
+    
+    }
+
+    const h2Text = document.createTextNode('Myhzy Beats')
+    h2.classList.add('nav-title')
+    h2.appendChild(h2Text)
+
+    ul.classList.add('nav-links')
+
+    homeText = document.createTextNode('Home')
+    beatsText = document.createTextNode('Beats')
+    contactText = document.createTextNode('Contact')
+
+    content.appendChild(nav)
+    nav.appendChild(h2)
+    nav.appendChild(ul)
+    ul.appendChild(newli())
+    liHTML.appendChild(homeText)
+    ul.appendChild(newli())
+    liHTML.appendChild(beatsText)
+    
+
+
+
+}
