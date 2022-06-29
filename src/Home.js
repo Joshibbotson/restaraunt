afaeb8d5d0d7677e3bd1d8b67c1e63d93d562ea6
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2022-06-29 19:15:39 +0100

    updates

---
+++ src/Home.js
@@ -0,0 +1,45 @@
+homeDomElement = {
+    content: document.getElementById('content'),
+    nav: document.createElement('nav'),
+    header: document.createElement('header'),
+    main: document.createElement('main'),
+    div: document.createElement('div'),
+    li: document.createElement('li'),
+    ul: document.createElement('ul'),
+    h2: document.createElement('h2'),
+    liHTML: document.getElementsByTagName('li')
+}
+
+home()
+function home () {
+
+    function newli() {
+        return document.createElement('li')
+    
+    }
+
+    const h2Text = document.createTextNode('Myhzy Beats')
+    h2.classList.add('nav-title')
+    h2.appendChild(h2Text)
+
+    homeDomElement.ul.classList.add('nav-links')
+
+    homeText = document.createTextNode('Home')
+    beatsText = document.createTextNode('Beats')
+    contactText = document.createTextNode('Contact')
+
+    homeDomElement.content.appendChild(nav)
+    homeDomElement.nav.appendChild(h2)
+    homeDomElement.nav.appendChild(ul)
+    homeDomElement.ul.appendChild(newli())
+
+    homeDomElement.ul.appendChild(newli())
+    
+
+
+
+}
+
+
+export {homeDomElement}
+export {home}
--- src/index.js
+++ src/index.js
@@ -1,42 +1,4 @@
-const content = document.getElementById('content')
+// import {homeDomElement} from './Home.js'
+// import { home } from './Home.js'
 
-const nav = document.createElement('nav')
-const header = document.createElement('header')
-const main = document.createElement('main')
-const div = document.createElement('div')
-const li = document.createElement('li')
-const ul = document.createElement('ul')
-const h2 = document.createElement('h2')
-const liHTML = document.getElementsByTagName('li')
-
-
-home()
-function home () {
-
-    function newli() {
-        return document.createElement('li')
-    
-    }
-
-    const h2Text = document.createTextNode('Myhzy Beats')
-    h2.classList.add('nav-title')
-    h2.appendChild(h2Text)
-
-    ul.classList.add('nav-links')
-
-    homeText = document.createTextNode('Home')
-    beatsText = document.createTextNode('Beats')
-    contactText = document.createTextNode('Contact')
-
-    content.appendChild(nav)
-    nav.appendChild(h2)
-    nav.appendChild(ul)
-    ul.appendChild(newli())
-    liHTML.appendChild(homeText)
-    ul.appendChild(newli())
-    liHTML.appendChild(beatsText)
-    
-
-
-
-}
+// home()
